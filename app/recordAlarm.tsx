d47736b85ca94e50e4a9dad052bd5af0ee91889e
Andres Felipe Velez Velez <af.velezv1@uniandes.edu.co>
2025-09-23 14:44:59 -0500

    chore: refactor RecordAlarmScreen layout and styles for improved structure

--- app/recordAlarm.tsx
+++ app/recordAlarm.tsx
@@ -1,39 +1,51 @@
 import { ThemedText } from "@/components/themed-text";
 import { ThemedView } from "@/components/themed-view";
+import BackgroundWrapper from "@/components/ui/backgroundWrapper";
 import { Ionicons } from "@expo/vector-icons";
 import { Link, router } from "expo-router";
 import React from "react";
-import { StyleSheet, TouchableOpacity } from "react-native";
+import { StyleSheet, TouchableOpacity, View } from "react-native";
 
 export default function RecordAlarmScreen() {
   return (
-    <ThemedView style={styles.container}>
-      {/* Ícono del micrófono */}
-      <ThemedView style={styles.micContainer}>
-        <Ionicons name="mic" size={120} color="#007BFF" />
-        <ThemedText style={styles.waveText}>- - | - -</ThemedText>
-      </ThemedView>
+    <BackgroundWrapper>
+      <ThemedView style={styles.container}>
+        {/* Ícono del micrófono */}
+        <ThemedView style={styles.micContainer}>
+          <Ionicons name="mic" size={120} color="#007BFF" />
+        </ThemedView>
 
-      {/* Texto descriptivo */}
-      <ThemedText style={styles.description}>
-        Configura la alarma de las 5:30 a.m. con la canción{" "}
-        <ThemedText style={styles.bold}>milagros de Karol G</ThemedText> y con
-        aviso de linterna intermitente.
-      </ThemedText>
+        {/* Waves */}
+        <View style={styles.waveContainer}>
+          <View style={[styles.waveBar, { height: 20 }]} />
+          <View style={[styles.waveBar, { height: 40 }]} />
+          <View style={[styles.waveBar, { height: 20 }]} />
+          <View style={[styles.waveBar, { height: 70 }]} />
+          <View style={[styles.waveBar, { height: 40 }]} />
+          <View style={[styles.waveBar, { height: 20 }]} />
+        </View>
 
-      {/* Botón cancelar */}
-      <TouchableOpacity
-        style={styles.cancelButton}
-        onPress={() => router.back()}
-      >
-        <ThemedText style={styles.cancelButtonText}>Cancelar</ThemedText>
-      </TouchableOpacity>
-      <Link href="/recordAlarmCheck" asChild>
-        <TouchableOpacity style={styles.cancelButton}>
-          <ThemedText style={styles.cancelButtonText}>Guardar</ThemedText>
+        {/* Texto descriptivo */}
+        <ThemedText type="title" style={styles.description}>
+          Configura la alarma de las 5:30 a.m. con la canción milagros de Karol
+          G y con aviso de linterna intermitente.
+        </ThemedText>
+
+        {/* Botón cancelar */}
+        <TouchableOpacity
+          style={styles.cancelButton}
+          onPress={() => router.back()}
+        >
+          <ThemedText style={styles.cancelButtonText}>Cancelar</ThemedText>
         </TouchableOpacity>
-      </Link>
-    </ThemedView>
+
+        <Link href="/recordAlarmCheck" asChild>
+          <TouchableOpacity style={styles.cancelButton}>
+            <ThemedText style={styles.cancelButtonText}>Guardar</ThemedText>
+          </TouchableOpacity>
+        </Link>
+      </ThemedView>
+    </BackgroundWrapper>
   );
 }
 
@@ -44,6 +56,7 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     padding: 20,
     gap: 40,
+    backgroundColor: "transparent",
   },
   micContainer: {
     alignItems: "center",
@@ -52,18 +65,27 @@ const styles = StyleSheet.create({
     borderColor: "#007BFF",
     borderRadius: 30,
     padding: 30,
+    backgroundColor: "transparent",
   },
-  waveText: {
-    marginTop: 16,
-    fontSize: 24,
-    color: "#007BFF",
-    fontWeight: "bold",
+  waveContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 12,
+    height: 70,
+    marginTop: -20,
+  },
+  waveBar: {
+    width: 18,
+    borderRadius: 3,
+    backgroundColor: "#007BFF",
   },
   description: {
-    fontSize: 16,
-    color: "#0C0C0C",
+    fontSize: 24,
     textAlign: "center",
     lineHeight: 22,
+    color: "#0C4A6E",
+    fontWeight: "600",
   },
   bold: {
     fontWeight: "700",
